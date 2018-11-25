14f6f2a008c361f007fb9135fe12427c12735987
Florentin <Flo@gmail.com>
2018-11-25 20:13:42 +0100

    Completer Pret
    DAO et Class Pret Modifier et completer

--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/PretDAO.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/PretDAO.java
@@ -1,5 +1,141 @@
 package be.pardons.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import be.pardons.POJO.Ex_Jeu;
+import be.pardons.POJO.Jeu;
+import be.pardons.POJO.Joueur;
+import be.pardons.POJO.Pret;
+
 public class PretDAO {
 
+	static Connection connec = JVConnection.getInstance();
+	Statement stmt = null;
+	ResultSet res = null;
+	
+	public PretDAO(){
+		//super(connec);
+	}
+	
+	/*
+	public PretDAO(Connection conn){
+		super(conn);
+	}
+	*/
+	
+	public boolean create(Pret pret, Joueur joueur){		
+		try
+		{
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+			stmt = connec.createStatement();
+			//#" + formatter.format(pret.GetDatesortie()) + "#
+			String insertion = "INSERT INTO Pret( num_joueur_FK, num_exjeu_FK, datedeb, datefin) values ('" + joueur.GetId() + "', '"+ pret.GetEx_Jeu().GetId() + "', #"  + formatter.format(pret.GetDateDeb()) + "#, #" + formatter.format(pret.GetDateFin()) + "#);";
+			System.out.println(insertion);
+			int res1 = stmt.executeUpdate(insertion);
+
+			if(res1 == 1) //Cree
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean delete(Pret pret){
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String del = "delete from Pret where num_pret_PK = '" + pret.GetId() + "';";
+			int res = stmt.executeUpdate(del);
+			
+			if(res == 1) //del ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean update(Pret pret){
+		try
+		{
+			/*
+			stmt = connec.createStatement();
+			
+			String upd = "update Pret set nom = '" + pret.GetNom() + "', tarif = '" + pret.GetTarif() + "', datesortie = '" + pret.GetDatesortie() + "', developpeur = '" + pret.GetDeveloppeur() + "', editeur = '" + pret.GetEditeur() + "'  where num_pret_PK = '" + pret.GetId() + "';";
+			int res = stmt.executeUpdate(upd);
+			
+			if(res == 1) //update ok
+			{
+				return true;
+			}*/
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean updatedate(Pret pret){
+		try
+		{
+			System.out.println(pret);
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+			stmt = connec.createStatement();
+			
+			String upd = "update Pret set datefin = #"  + formatter.format(pret.GetDateFin()) + "# where num_pret_PK = '" + pret.GetId() + "';";
+			System.out.println(upd);
+			int res = stmt.executeUpdate(upd);
+			
+			if(res == 1) //update ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public List<Pret> list(Joueur joueur){
+		List<Pret> list = new ArrayList<Pret>();
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String result = "SELECT Jeu.*, Pret.*, Ex_Jeu.* FROM (Jeu INNER JOIN Ex_Jeu ON Jeu.num_jeu_PK = Ex_Jeu.num_jeu_FK) INNER JOIN Pret ON Ex_Jeu.num_exjeux_PK = Pret.num_exjeu_FK where Pret.num_joueur_FK = " + joueur.GetId() +";";
+			res = stmt.executeQuery(result);
+			
+			while(res.next()) //verif pri
+			{
+				list.add(new Pret(res.getInt("Pret.num_pret_PK"), new Ex_Jeu(res.getInt("num_exjeux_PK"), new Jeu(res.getInt("num_jeu_PK"), res.getString("nom"), res.getInt("tarif"), res.getDate("datesortie"), res.getString("developpeur"), res.getString("editeur")), res.getBoolean("dispo")), res.getDate("datedeb"), res.getDate("datefin")));
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return list;
+	}
 }
--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Pret.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Pret.java
@@ -2,53 +2,99 @@ package be.pardons.POJO;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import be.pardons.DAO.PretDAO;
 
 public class Pret {
 
 	//Variable
-	private static int refBase = 0;
-	private String ref;
+	private int id;
 	private Date datedeb;
 	private Date datefin;
 	private Ex_Jeu exjeu;
 	
 	//Setteur
-	public void SetRef(String ref) {
-		this.ref = ref;
+	public void SetId(int id) {
+		this.id = id;
+	}
+	public void SetDateDeb(Date datedeb) {
+		this.datedeb = datedeb;
+	}
+	public void SetDateFin(Date datefin) {
+		this.datefin = datefin;
+	}
+	public void SetEx_Jeu(Ex_Jeu exjeu) {
+		this.exjeu = exjeu;
 	}
 	
 	//Getteur
-	public String GetRef() {
-		return this.ref;
+	public int GetId() {
+		return this.id;
+	}
+	public Date GetDateDeb() {
+		return this.datedeb;
+	}
+	public Date GetDateFin() {
+		return this.datefin;
+	}
+	public Ex_Jeu GetEx_Jeu() {
+		return this.exjeu;
 	}
 	
 	//Constructeur
-	public Pret(String ref, Date datedeb, Date datefin, Ex_Jeu exjeu)
+	public Pret(int id, Ex_Jeu exjeu, Date datedeb, Date datefin)
 	{
-		this.ref = ref;
+		this.id = id;
+		this.exjeu = exjeu;
 		this.datedeb = datedeb;
 		this.datefin = datefin;
-		this.exjeu = exjeu;
 	}
 	
-	public Pret(int datefin, Ex_Jeu exjeu)
+	public Pret(Ex_Jeu exjeu)
 	{
-		this.ref = "&" + refBase;
-		refBase++;
+		this.exjeu = exjeu;
 		this.datedeb = new Date();
 		
-		//Add
 		Calendar c = Calendar.getInstance();
-		c.setTime(new Date());
-		c.add(Calendar.DATE, datefin*7);
+		c.setTime(datedeb);
+		c.add(Calendar.DATE, 7);
 		
 		this.datefin = c.getTime();
-		this.exjeu = exjeu;
+		
 	}
 	
+	//Methode
+	
+	//Creation de la liste
+	public static List<Pret> List(Joueur joueur)
+	{
+		PretDAO pretDao = new PretDAO();
+		return pretDao.list(joueur);
+	}
+	
+	public boolean Creer(Joueur joueur) {
+		PretDAO pretDao = new PretDAO();
+		return pretDao.create(this, joueur);
+	}
+	
+	public boolean Delete() {
+		PretDAO pretDao = new PretDAO();
+		return pretDao.delete(this);
+	}
+	
+	public boolean Update() {
+		PretDAO pretDao = new PretDAO();
+		return pretDao.update(this);
+	}
+	
+	public boolean UpdateDate() {
+		PretDAO pretDao = new PretDAO();
+		return pretDao.updatedate(this);
+	}
+		
 	// Tostring
 	@Override
 	public String toString() {
-		return ref + " " + datedeb + " " + datefin + " " + exjeu ;
+		return id + " " + datedeb + " " + datefin + " " + exjeu ;
 	}
 }
