83b6cc9caa905e8692544783427ae2d60d74a99e
Florentin <Flo@gmail.com>
2018-11-25 20:05:19 +0100

    Completer Joueur
    DAO et Class Joueur completer et modifier

--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/JoueurDAO.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/JoueurDAO.java
@@ -1,5 +1,181 @@
 package be.pardons.DAO;
 
-public class JoueurDAO {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import be.pardons.POJO.Joueur;
 
+public class JoueurDAO extends DAO<Joueur>{
+	
+	static Connection connec = JVConnection.getInstance();
+	Statement stmt = null;
+	ResultSet res = null;
+	
+	public JoueurDAO(){
+		super(connec);
+	}
+	
+	public JoueurDAO(Connection conn){
+		super(conn);
+	}
+	
+	public boolean create(Joueur joueur){		
+		try
+		{			
+			stmt = connec.createStatement();
+			
+			String insertion = "Insert into Joueur(num_joueur_PK, solde) values ((SELECT num_pers_PK from personne WHERE pseudo='" + joueur.GetPseudo() + "')" + ", '" + joueur.GetSolde() + "');";
+			int res = stmt.executeUpdate(insertion);
+			
+			if(res == 1) //Cree
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+
+	public Joueur verif(String pseudo, String mp){
+		Joueur joueur = new Joueur();
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String verif = "SELECT Personne.*, Joueur.* FROM Personne INNER JOIN Joueur ON Personne.num_pers_PK = Joueur.num_joueur_PK where Personne.pseudo = '" + pseudo + "' and Personne.mp = '" + mp +"';";
+			res = stmt.executeQuery(verif);			
+			
+			if(res.next()) //verif pri
+			{
+				joueur.SetId(res.getInt("Personne.num_pers_PK"));
+				joueur.SetPseudo(res.getString("Personne.pseudo"));
+				joueur.SetNom(res.getString("Personne.nom"));
+				joueur.SetPrenom(res.getString("Personne.prenom"));
+				joueur.SetAge(res.getInt("Personne.age"));
+				joueur.SetAdresse(res.getString("Personne.adresse"));
+				joueur.SetSolde(res.getDouble("Joueur.solde"));
+			}			
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return joueur;
+	}
+	
+	public boolean delete(Joueur joueur){
+		try
+		{			
+			stmt = connec.createStatement();
+			
+			String del = "delete from Joueur where num_joueur_PK = '" + joueur.GetId() + "';";
+			
+			int res = stmt.executeUpdate(del);
+			
+			if(res == 1) //del ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean update(Joueur joueur){
+		try
+		{			
+			stmt = connec.createStatement();
+			
+			String upd = "update Joueur set solde = '" + joueur.GetSolde() + "where num_joueur_PK = '" + joueur.GetId() +"';";
+			res = stmt.executeQuery(upd);
+			
+			if(res.next()) //update ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean updatesolde(Joueur joueur){
+		try
+		{	
+			System.out.println(joueur);
+			stmt = connec.createStatement();
+			
+			String upd = "update Joueur set solde = " + joueur.GetSolde() + " where num_joueur_PK = " + joueur.GetId() + ";";
+			System.out.println(upd);
+			int res = stmt.executeUpdate(upd);
+			
+			if(res == 1) //update ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public Joueur find(String pseudo){
+		Joueur joueur = new Joueur();
+		try
+		{
+			PersonneDAO pers = new PersonneDAO();
+			int id = pers.findId(pseudo);
+			stmt = connec.createStatement();
+			
+			String find = "SELECT * FROM Joueur WHERE num_joueur_PK = '" + id +"';";
+			res = stmt.executeQuery(find);
+			
+			if(res.first()) //update ok
+			{
+				//pers = new Joueur(res.getString("pseudo"), res.getString("mp"), res.getString("nom"), res.getString("prenom"), res.getInt("age"), res.getString("adresse"));
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		return joueur;
+	}
+	
+	public List<Joueur> list(){
+		List<Joueur> list = new ArrayList<Joueur>();
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String result = "SELECT Joueur.solde, Personne.* FROM Personne INNER JOIN Joueur ON Personne.num_pers_PK = Joueur.num_joueur_PK;";
+			res = stmt.executeQuery(result);
+			
+			while(res.next()) //verif pri
+			{
+				list.add(new Joueur(res.getDouble("Joueur.solde"), res.getInt("Personne.num_pers_PK"), res.getString("Personne.pseudo"), res.getString("Personne.mp"), res.getString("Personne.nom"), res.getString("Personne.prenom"), res.getInt("Personne.age"), res.getString("Personne.adresse")));
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return list;
+	}
 }
--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Joueur.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Joueur.java
@@ -1,32 +1,33 @@
 package be.pardons.POJO;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import be.pardons.DAO.JoueurDAO;
+import be.pardons.DAO.PersonneDAO;
 
 public class Joueur extends Personne {
 
 	//Variable
 	private double solde;
-	private List<Ex_Jeu> listJeu;
+	private List<Ex_Jeu> listExJeu;
 	private List<Reservation> listRes;
 	private List<Pret> listPret;
 	
-	private Date date_enr;
-	private boolean rayer;
+	//private Date date_enr;
+	//private boolean rayer;
 	
-	//Setteur
+	//Setteur	
 	public void SetSolde(double solde) {
 		this.solde = solde;
-	}
+	}/*
 	public void SetDate_enr(Date date_enr) {
 		this.date_enr = date_enr;
 	}
 	public void SetRayer(boolean rayer) {
 		this.rayer = rayer;
-	}
-	public void SetListJeu(List<Ex_Jeu> listJeu) {
-		this.listJeu = listJeu;
+	}*/
+	public void SetListJeu(List<Ex_Jeu> listExJeu) {
+		this.listExJeu = listExJeu;
 	}
 	public void SetListRes(List<Reservation> listRes) {
 		this.listRes = listRes;
@@ -38,15 +39,15 @@ public class Joueur extends Personne {
 	//Getteur
 	public double GetSolde() {
 		return this.solde;
-	}
+	}/*
 	public Date GetDate_enr() {
 		return this.date_enr;
 	}
 	public boolean GetRayer() {
 		return this.rayer;
-	}
+	}*/
 	public List<Ex_Jeu> GetListJeu() {
-		return listJeu;
+		return listExJeu;
 	}
 	public List<Reservation> GetListRes() {
 		return listRes;
@@ -57,51 +58,103 @@ public class Joueur extends Personne {
 	
 	//Constructeur
 	public Joueur()
-	{}
+	{
+		this.listExJeu = new ArrayList<Ex_Jeu>();
+		this.listRes = new ArrayList<Reservation>();
+		this.listPret = new ArrayList<Pret>();
+	}
 
-	public Joueur(double solde, Date date_enr, boolean rayer, String pseudo, String mp, String nom, String prenom, Integer age, String adresse)
+	public Joueur(String pseudo, String mp, String nom, String prenom, Integer age, String adresse)
 	{
 		super(pseudo, mp, nom, prenom, age, adresse);
-		this.solde = solde;
-		this.date_enr = date_enr;
-		this.rayer = rayer;
-		this.listJeu = new ArrayList<Ex_Jeu>();
+		this.solde = 5;/*
+		this.date_enr = new Date();
+		this.rayer = false;*/
+		this.listExJeu = new ArrayList<Ex_Jeu>();
 		this.listRes = new ArrayList<Reservation>();
 		this.listPret = new ArrayList<Pret>();
 	}
 	
-	public Joueur(String pseudo, String mp, String nom, String prenom, Integer age, String adresse)
+	public Joueur(double solde, int id, String pseudo, String mp, String nom, String prenom, Integer age, String adresse)
 	{
-		super(pseudo, mp, nom, prenom, age, adresse);
-		this.solde = 5.0;
+		super(id, pseudo, mp, nom, prenom, age, adresse);
+		this.solde = solde;/*
+		this.date_enr = date_enr;
+		this.rayer = rayer;*/
+		this.listExJeu = new ArrayList<Ex_Jeu>();
+		this.listRes = new ArrayList<Reservation>();
+		this.listPret = new ArrayList<Pret>();
+	}
+	
+	public Joueur(int id, String pseudo, String mp, String nom, String prenom, Integer age, String adresse)
+	{
+		super(id, pseudo, mp, nom, prenom, age, adresse);
+		this.solde = 5.0;/*
 		this.date_enr = null;
-		this.rayer = false;
-		this.listJeu = new ArrayList<Ex_Jeu>();
+		this.rayer = false;*/
+		this.listExJeu = new ArrayList<Ex_Jeu>();
 		this.listRes = new ArrayList<Reservation>();
 		this.listPret = new ArrayList<Pret>();
 	}
 	
 	//Ajouter un jeu
-	public void AddExJeu(Jeu jeu, String ref, boolean dispo)
+	/*
+	public void AddExJeu(Jeu jeu, boolean dispo)
 	{
-		listJeu.add(new Ex_Jeu(jeu, ref, dispo));
-	}
-	
+		listExJeu.add(new Ex_Jeu(jeu, dispo));
+	}*/
+	/*
 	//Ajouter une reservation
 	public void AddRes(Jeu jeu)
 	{
 		listRes.add(new Reservation(jeu));
-	}
+	}*/
 	
 	//Ajouter un pret
-	public void AddPret(int datefin, Ex_Jeu exjeu)
+	/*public void AddPret(int id, Ex_Jeu exjeu, Date deb, Date )
 	{
 		listPret.add(new Pret(datefin, exjeu));
+	}*/
+	
+	//Verification Connexion
+	public static Joueur Verif(String pseudo, String mp)
+	{
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.verif(pseudo, mp);
+	}
+	
+	//Creation de la liste
+	public static List<Joueur> List()
+	{
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.list();
+	}
+	
+	public boolean Creer() {
+		PersonneDAO persDao = new PersonneDAO();
+		persDao.create(this);
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.create(this);
+	}
+	
+	public boolean Delete() {
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.delete(this);
+	}
+	
+	public boolean Update() {
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.update(this);
+	}
+	
+	public boolean UpdateSolde() {
+		JoueurDAO joueurDao = new JoueurDAO();
+		return joueurDao.updatesolde(this);
 	}
 	
 	// Tostring
 	@Override
 	public String toString() {
-		return super.toString() + " " + solde + " " + date_enr + " " + rayer;
+		return super.toString() + " " + solde;
 	}
 }
