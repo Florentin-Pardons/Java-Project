8520c27c53f66031506d46998237d414808de158
Florentin <Flo@gmail.com>
2018-11-25 20:15:05 +0100

    Completer Reservation
    DAO et Class Reservation Completer et Modifier

--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/ReservationDAO.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/DAO/ReservationDAO.java
@@ -1,5 +1,116 @@
 package be.pardons.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import be.pardons.POJO.Jeu;
+import be.pardons.POJO.Joueur;
+import be.pardons.POJO.Reservation;
+
 public class ReservationDAO {
 
+	static Connection connec = JVConnection.getInstance();
+	Statement stmt = null;
+	ResultSet res = null;
+	
+	public ReservationDAO(){
+		//super(connec);
+	}
+	
+	/*
+	public ReservationDAO(Connection conn){
+		super(conn);
+	}
+	*/
+	
+	public boolean create(Reservation reservation, Joueur joueur){		
+		try
+		{
+			SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+			stmt = connec.createStatement();
+			//#" + formatter.format(reservation.GetDatesortie()) + "#
+			String insertion = "INSERT INTO Reservation( num_joueur_FK, num_jeu_FK, dateRes ) values ('" + joueur.GetId() + "', '" + reservation.GetJeu().GetId() + "', #"  + formatter.format(reservation.GetDateRes()) + "#);";
+			System.out.println(insertion);
+			int res1 = stmt.executeUpdate(insertion);
+
+			if(res1 == 1) //Cree
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean delete(Reservation reservation){
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String del = "delete from Reservation where num_res_PK = '" + reservation.GetId() + "';";
+			int res = stmt.executeUpdate(del);
+			
+			if(res == 1) //del ok
+			{
+				return true;
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public boolean update(Reservation reservation){
+		try
+		{/*
+			stmt = connec.createStatement();
+			
+			String upd = "update Reservation set nom = '" + reservation.GetNom() + "', tarif = '" + reservation.GetTarif() + "', datesortie = '" + reservation.GetDatesortie() + "', developpeur = '" + reservation.GetDeveloppeur() + "', editeur = '" + reservation.GetEditeur() + "'  where num_reservation_PK = '" + reservation.GetId() + "';";
+			int res = stmt.executeUpdate(upd);
+			
+			if(res == 1) //update ok
+			{
+				return true;
+			}*/
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return false;
+	}
+	
+	public List<Reservation> list(Joueur joueur){
+		List<Reservation> list = new ArrayList<Reservation>();
+		try
+		{
+			stmt = connec.createStatement();
+			
+			String result = "SELECT Jeu.*, Reservation.* FROM Jeu INNER JOIN Reservation ON Jeu.num_jeu_PK = Reservation.num_jeu_FK where Reservation.num_joueur_FK = " + joueur.GetId() + ";";
+			res = stmt.executeQuery(result);
+			
+			while(res.next()) //verif pri
+			{
+				list.add(new Reservation(res.getInt("Reservation.num_res_PK"), res.getDate("dateres"), new Jeu(res.getInt("Jeu.num_jeu_PK"), res.getString("Jeu.nom"), res.getInt("Jeu.tarif"), res.getDate("Jeu.datesortie"), res.getString("Jeu.developpeur"), res.getString("Jeu.editeur"))));
+			}
+		}
+		catch(Exception err)
+		{
+			System.out.println(err);
+		}
+		
+		return list;
+	}
 }
--- Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Reservation.java
+++ Workspace-Projet-JV/Projet_Jeux_Video/src/be/pardons/POJO/Reservation.java
@@ -1,44 +1,106 @@
 package be.pardons.POJO;
 
 import java.util.Date;
+import java.util.List;
+import be.pardons.DAO.ReservationDAO;
 
 public class Reservation {
 
 	//Variable
-	private static int refBase = 0;
-	private String ref;
+	private int id;
 	private Date dateres ;
 	private Jeu jeu;
 	
 	//Getteur
-	public String GetRef() {
-		return ref;
+	public int GetId() {
+		return id;
+	}
+	public Date GetDateRes() {
+		return dateres;
+	}
+	public Jeu GetJeu() {
+		return jeu;
 	}
 	
 	//Setteur
-	public void SetRef(String ref) {
-		this.ref = ref;
+	public void SetId(int id) {
+		this.id = id;
+	}
+	public void SetDateRes(Date dateres) {
+		this.dateres = dateres;
+	}
+	public void SetJeu(Jeu jeu) {
+		this.jeu = jeu;
 	}
 	
 	//Constructeur
-	public Reservation(String ref, Date dateres, Jeu jeu)
+	public Reservation(int id, Date dateres, Jeu jeu)
 	{
-		this.ref = ref;
+		this.id = id;
 		this.dateres = dateres;
 		this.jeu = jeu;
 	}
 	
 	public Reservation(Jeu jeu)
 	{
-		this.ref = "&" + refBase;
-		refBase++;
 		this.dateres = new Date();
 		this.jeu = jeu;
 	}
 	
+	//Methode
+	
+	//Creation de la liste
+	public static List<Reservation> List(Joueur joueur)
+	{
+		ReservationDAO reservationDao = new ReservationDAO();
+		return reservationDao.list(joueur);
+	}
+	
+	public boolean Creer(Joueur joueur) {
+		ReservationDAO reservationDao = new ReservationDAO();
+		return reservationDao.create(this, joueur);
+	}
+	
+	public boolean Delete() {
+		ReservationDAO reservationDao = new ReservationDAO();
+		return reservationDao.delete(this);
+	}
+	
+	public boolean Update() {
+		ReservationDAO reservationDao = new ReservationDAO();
+		return reservationDao.update(this);
+	}
+	
+	//Verifie les reservations
+	public boolean Verif(Joueur joueur)
+	{
+		if(joueur.GetSolde() > this.GetJeu().GetTarif()) //verifie le solde
+		{
+			Ex_Jeu exjeu = Ex_Jeu.Dispo(this.GetJeu()); //si il y a un ex dispo
+			
+			if(exjeu != null)
+			{
+				Pret pret = new Pret(exjeu);
+				if(pret.Creer(joueur) == true) //creer pret
+				{
+					exjeu.SetDispo(false);
+					exjeu.Update(); //update dispo
+					
+					//solde
+					double newsolde = joueur.GetSolde() - this.GetJeu().GetTarif();
+					joueur.SetSolde(newsolde);
+					joueur.UpdateSolde();
+					
+					return true;
+				}	
+			}
+		}
+		return false;
+	}
+		
 	// Tostring
 	@Override
 	public String toString() {
-		return ref + " " + dateres + " " + jeu;
+		return id + " " + dateres + " " + jeu;
 	}	
 }
